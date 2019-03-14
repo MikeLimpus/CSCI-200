e9cae410ad3aeec2e5b9d54abb1daac944e0ed14
Mike Limpus <mikelimpus111@gmail.com>
2019-03-14 00:23:54 -0700

    Problem 1 and CartProd dependancy

---
+++ Homework/Homework2/Octave/cartProd.m
@@ -0,0 +1,11 @@
+function retval = cartProd (varargin)
+  if (nargin < 1)
+    print_usage ();
+  elseif (nargin == 1)
+    retval = varargin{1};
+  endif
+
+  [retval{1:nargin}] = ndgrid (varargin{:});
+  retval = cat (nargin+1, retval{:});
+  retval = reshape (retval, [], nargin);
+endfunction
---
+++ Homework/Homework2/Octave/problem1.m
@@ -0,0 +1,14 @@
+
+% Cartesian product function as defined in linear-algebra package
+
+A = [1 2 3] 
+B = [4 5] 
+C = [1 6 7] 
+
+disp("A x B x C")
+cartProd(A, B, C)
+disp("\n")
+disp("B x B x B") 
+cartProd(B, B, B) 
+
+

ba86442501a3057f242f80faf36eaa855ca9a972
Mike Limpus <mikelimpus111@gmail.com>
2019-03-26 21:41:19 -0700

    Homework 3

---
+++ Homework/Homework3/problem1.py
@@ -0,0 +1,10 @@
+sequence = [1, 1, 2, 12, 3, 8, 100, 1000]
+new_sequence = []
+for i in range(sequence[2], len(sequence)):
+    foo = sum(sequence[0, i - 1])
+    if (sequence[i] > foo):
+        new_sequence.append(sequence[i])
+print(sequence)
+print(new_sequence)
+
+  
